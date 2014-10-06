f2184ace3204a63093d31ba7460c239096df3822
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-06 14:55:30 -0400

    Updating sync pods

--- build/sync-pods.sh
+++ build/sync-pods.sh
@@ -24,6 +24,7 @@ echo Done web-pods-common
 
 echo Synching web-pods-datasource
 sync_dir datasource $HGDIR/web-pods-datasource/src/main/java/org/diirt ../core/plugins/org.diirt.datasource.pods.web/src/org/diirt/
+sync_dir datasource $HGDIR/web-pods-datasource/src/main/resources/org/diirt ../core/plugins/org.diirt.datasource.pods.web/resources/org/diirt/
 git commit --author="Gabriele Carcassi <gabriele.carcassi@gmail.com>" -m "org.diirt.datasource.pods.web: update to current SNAPSHOT" ../core/plugins/org.diirt.datasource.pods.web
 echo Done web-pods-datasource
 
