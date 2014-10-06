88bfd03c7f178da516667b4c44a2a6e8dd208711
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-06 14:46:53 -0400

    org.diirt.datasource.pods.web: update to current SNAPSHOT

--- core/plugins/org.diirt.datasource.pods.web/src/org/diirt/datasource/pods/web/WebPodsDataSource.java
+++ core/plugins/org.diirt.datasource.pods.web/src/org/diirt/datasource/pods/web/WebPodsDataSource.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
  * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
 package org.diirt.datasource.pods.web;
@@ -15,11 +15,11 @@ import javax.websocket.WebSocketContainer;
 
 import org.epics.pvmanager.ChannelHandler;
 import org.epics.pvmanager.DataSource;
+import org.epics.pvmanager.PVManager;
 import org.epics.pvmanager.vtype.DataTypeSupport;
 
 /**
- * Data source for locally written data. Each instance of this
- * data source will have its own separate channels and values.
+ * Data source for web=pods.
  *
  * @author carcassi
  */
@@ -31,19 +31,33 @@ public final class WebPodsDataSource extends DataSource {
     }
     
     private final WebPodsClient client;
+    private final WebPodsDataSourceConfiguration configuration;
+
+    public WebPodsDataSource() {
+        this(WebPodsDataSourceConfiguration.readConfiguration(new WebPodsDataSourceFactory().getDefaultConfPath()));
+    }
 
     /**
      * Creates a new data source.
+     * 
+     * @param configuration datasource configuration
      */
-    public WebPodsDataSource() {
+    public WebPodsDataSource(WebPodsDataSourceConfiguration configuration) {
         super(true);
-        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         client = new WebPodsClient();
-        try {
-            container.connectToServer(client, new URI("ws://localhost:8080/web-pods/socket"));
-        } catch (DeploymentException | IOException | URISyntaxException ex) {
-            Logger.getLogger(WebPodsDataSource.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        this.configuration = configuration;
+        PVManager.getReadScannerExecutorService().execute(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+                    container.connectToServer(client, WebPodsDataSource.this.configuration.getSocketLocation());
+                } catch (DeploymentException | IOException ex) {
+                    Logger.getLogger(WebPodsDataSource.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
     }
 
     @Override
